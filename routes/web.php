c099caa74e346752a2e3763815f93a4e21c34ea5
pudinmah <pudinmah69@gmail.com>
2024-12-30 10:18:06 +0700

    ui front startbootsrap

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front.index');
 });
 
 Route::get('/dashboard', function () {
-    // return view('dashboard');
     return view('admin.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
